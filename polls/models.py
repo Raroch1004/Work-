9d21cf2cc7d240eaeea8a8e95467d13de4d60176
Daniil <EaseJeat1999_1004@mail.ru>
2024-07-04 12:59:40 +0300

    Исправлены ошибки в форматировании

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+
+
     def __str__(self):
         return self.question_text
     @admin.display(
@@ -12,6 +14,7 @@ class Question(models.Model):
         ordering="pub_date",
         description="Published recently?",
     )
+
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
